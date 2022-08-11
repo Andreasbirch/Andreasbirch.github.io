5e3c893a969c44a5985f7e70f31d48db11fe5dd8
Andreasbirch <55704784+Andreasbirch@users.noreply.github.com>
2022-08-11 19:07:44 +0200

    Added graph algos to display string as tree

--- apps/tableaux/scripts.js
+++ apps/tableaux/scripts.js
@@ -1,21 +1,26 @@
 function interpretInput(input) {
-    parenthesise(input);
-
-
+    // parenthesise(input);
+    var parsedObj = parse(input);
+    console.log(parsedObj);
+    resizeRecursion(parsedObj);
+    console.log(parsedObj);
+    parsedObjToDivs(parsedObj);
+    console.log(treeStr);
+    
     output = input.replaceAll("!","¬");
     output = input.replaceAll("&", "∧");
     output = input.replaceAll("|", "∨");
     output = input.replaceAll("<>", "↔");
     output = input.replaceAll("->", "→");
 
-    $('#interpretation').text(output);
+    // $('#interpretation').text(output);
+    $('#display').append(treeStr);
 }
 
 function preset(input) {
     $('#input').val(input);
     interpretInput(input);
 }
-
 
 function parenthesise(input) {
     input = input.replaceAll(" ", "");
@@ -30,29 +35,68 @@ function parenthesise(input) {
     // console.log("Parenthesised: " + input);
 }
 
-function precedence(input, statement) {
-    //Goes through symbol precedence list. Might have to check up on equality of & and | if such exists.
-    var symbol = symbols.find(s => input.includes(s));
-
-    if(input.length == 1 || (input.length == 2 && symbol == "!")) {
-        return statement;
+function parseRecursion(input, parent) {
+    if(input.length <= 1 || (input.length <= 2 && input.includes("!"))){
+        parent.children.push(null);
     } else {
-        substrings = input.split(symbol);
-        // console.log(substrings[0] + "  -  " + substrings[1]);
-        statement.children.push(substrings);
-        substrings.slice(0,-1).forEach(substring => {
-            var subStatement = new Statement(substring);
-            subStatement = precedence(substring, subStatement);
-            statement.children.push(subStatement);
+        var currSym = symbols.find(s => input.includes(s));
+        input.split(currSym).forEach(function (substr) {
+            const chld = {
+                value: substr,
+                width: 5,
+                children: []
+            };
+            parseRecursion(substr, chld);
+            parent.children.push(chld);
         });
     }
 }
 
-class Statement {
-    constructor(value) {
-        this.value = value;
-        this.children = [];
+function parse() {
+    var input = "p->q&!r";
+    const baseSentence = {
+        value: input,
+        width: input.length,
+        children: []
     }
+    parseRecursion(input, baseSentence);
+    return baseSentence;
+}
+
+function resizeRecursion(parent) {
+    var childWidth = 0;
+    parent.children.forEach(function (child) {
+        if(child == null){
+
+        } else {
+            resizeRecursion(child);
+            childWidth += child.width;
+        }
+    });
+    if(childWidth > parent.width){
+        parent.width = childWidth;
+    }
+}
+
+var treeStr = "";
+
+
+function parsedObjToDivs(parent) {
+    treeStr = treeStr.concat(`<div style="width:${parent.width * 10}px; float: left; text-align: center;"><p>${parent.value}</p>`);
+    dfs(parent);
+    treeStr = treeStr.concat("</div>");
+}
+
+function dfs(parent) {
+    parent.children.forEach(function (child) {
+        if(child == null){
+
+        } else {
+            treeStr = treeStr.concat(`<div style="width:${child.width * 10}px; float: left; text-align: center;"><p>${child.value}</p>`);
+            dfs(child);
+            treeStr = treeStr.concat("</div>");
+        }
+    });
 }
 
 const symbols = ["<>", "->", "|", "&", "!"];
