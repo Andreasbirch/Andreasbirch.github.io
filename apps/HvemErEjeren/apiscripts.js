2d907b95d12800dc314bdf88e06ac445561ed0a1
Andreas Birch <andreasbirch1998@hotmail.com>
2024-02-26 15:17:52 +0100

    Added function to api call, but cors error :(

--- apps/HvemErEjeren/apiscripts.js
+++ apps/HvemErEjeren/apiscripts.js
@@ -1,3 +1,16 @@
 function getOwnersOfCompany(cvr) {
-
+    fetch('http://distribution.virk.dk/cvr-permanent/virksomhed/_search', {
+        method:'POST', 
+        headers: {
+            'Authorization': 'Basic ' + btoa(`${userid}:${password}`)
+        },
+        body: JSON.stringify({
+            "query": {
+                "term": {
+                    "Vrvirksomhed.cvrNummer": `${cvr}`
+                }
+            }
+        })
+    }).then(response => response.json())
+    .then(json => console.log(json));
 }
